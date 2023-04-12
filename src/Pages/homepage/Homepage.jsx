a7791d0f86cc0b5a8e609dd69e55dc8af9cf21c0
nibasstha <sabinshrestha4664@gmail.com>
2023-04-12 06:41:27 +0545

    setup

---
+++ src/Pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+//rafce to create format
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+
+const Homepage = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>Homepage</h1>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/Pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+
+const Login = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>login</h1>
+    </div>
+  )
+}
+
+export default Login
